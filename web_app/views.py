ff0e92d0b6b4304479ecbd1791fa9a975d6eb48c
rodcliffh <rodcliffh@gmail.com>
2021-03-17 21:22:23 -0400

    Delete web_app directory

--- web_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from requests import Request, post
-from django.shortcuts import render, redirect
-
-def home(request):
-    return render(request, 'spotify/home.html' )
-
-def signIn(request):
-    return render(request, 'spotify/signIn.html')
