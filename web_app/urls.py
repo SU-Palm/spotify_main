89a8c6d5aefc415874709286fd9a93af462930f3
rodcliffh <rodcliffh@gmail.com>
2021-03-17 21:23:26 -0400

    Add files via upload

---
+++ web_app/urls.py
@@ -0,0 +1,31 @@
+"""
+web_app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from spotify import views as v
+from .views import home, signIn
+
+urlpatterns = [
+    path('', home),
+    path('home/', home),
+    path('admin/', admin.site.urls),
+    path('spotify/', include('spotify.urls')),
+    path("login/", v.authentication, name="login"),
+    path("register/", v.register, name="register"),
+    path('signIn', signIn)
+]
+
