150a12567d8a3c369a3c4abc7ab6abcb4557b1a9
rodcliffh <rodcliffh@gmail.com>
2021-03-28 22:38:08 -0400

    Delete spotify directory

--- spotify/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import AuthURL, spotify_callback, IsAuthenticated, logIn
-
-urlpatterns = [
-    path('get-auth-url', AuthURL.as_view(), name="get-auth-url"),
-    path('redirect', spotify_callback),
-    path("logIn", logIn, name="logIn"),
-    path('is-authenticated', IsAuthenticated.as_view())
-]
